f7709f58013facf69e73d596d15383545a05c79a
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-10-04 23:57:18 +0200

    Created slurm script for collect_data.sh

---
+++ data-collection/slurm_collect_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=gather-pretraining-data # Job name
+#SBATCH --output=gather-pretraining-data.out # Name of output file
+#SBATCH --error=gather-pretraining-data.err # Name of error file
+#SBATCH --ntasks=1 # Number of tasks
+#SBATCH --cpus-per-task=2 # Number of CPU cores per task
+#SBATCH --time=1-00:00:00 # Wall time
+#SBATCH --mem-per-cpu=8000 # Memory per CPU core
+#SBATCH --mail-user=rpa@di.ku.dk # Email
+#SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT_90 # When to email
+
+cd ~/home/projects/thesis/thesis-code
+conda activate thesis
+bash data-collection/collect_data.sh ../data
