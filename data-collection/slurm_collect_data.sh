c149044823335d904d0a055258c6aeca54d2c715
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-10-05 12:14:18 +0200

    Changed core count to 4

--- data-collection/slurm_collect_data.sh
+++ data-collection/slurm_collect_data.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=gather-pretraining-data.out # Name of output file
 #SBATCH --error=gather-pretraining-data.err # Name of error file
 #SBATCH --ntasks=1 # Number of tasks
-#SBATCH --cpus-per-task=2 # Number of CPU cores per task
+#SBATCH --cpus-per-task=4 # Number of CPU cores per task
 #SBATCH --time=1-00:00:00 # Wall time
 #SBATCH --mem-per-cpu=8000 # Memory per CPU core
 #SBATCH --mail-user=rpa@di.ku.dk # Email
