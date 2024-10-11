e17495d438b559e4fd873e3a8374ca2dc0dbef7e
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-10-12 00:39:39 +0200

    added lightning module and fixed so it fit in with the package structure.

--- models/cicek_3d_vae.py
+++ models/cicek_3d_vae.py
@@ -4,6 +4,9 @@ from typing import Tuple
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import lightning as L
+
+from dataloading.mri_sample import MRISample
 
 
 class ConvUnit(nn.Module):
@@ -241,3 +244,52 @@ class VAE3D(nn.Module):
         else:
             beta = max_beta
         return beta
+
+
+class VAE3DLightningModule(L.LightningModule):
+    def __init__(
+        self,
+        in_shape: Tuple[int, int, int, int],
+        encoder_out_channels_per_block: list[int],
+        decoder_out_channels_per_block: list[int],
+        latent_dim: int,
+        pool_size: int = 2,
+        kernel_size: int = 2,
+        stride: int = 2,
+    ):
+        super().__init__()
+        # Save hyperparameters to the checkpoint
+        self.save_hyperparameters()
+
+        self.model = VAE3D(
+            in_shape,
+            encoder_out_channels_per_block,
+            decoder_out_channels_per_block,
+            latent_dim,
+            pool_size,
+            kernel_size,
+            stride,
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch: MRISample, batch_idx):
+        x, _ = batch  # Assuming your dataset returns (image, label)
+        recon_x, mu, log_var = self(x)
+
+        # Calculate loss
+        loss, recon_loss, kld_loss = self.model.calculate_loss(
+            x, recon_x, mu, log_var
+        )  # Assuming your model has this method
+
+        # Log losses
+        self.log("train_loss", loss)
+        self.log("recon_loss", recon_loss)
+        self.log("kld_loss", kld_loss)
+
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters())
+        return optimizer
