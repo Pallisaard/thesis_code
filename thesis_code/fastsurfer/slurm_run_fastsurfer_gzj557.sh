fa6381dd32e5d728b9f976685e4a6f1ecc7c76a2
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-10-25 13:04:27 +0200

    set array size to 5 jobs

--- thesis_code/fastsurfer/slurm_run_fastsurfer_gzj557.sh
+++ thesis_code/fastsurfer/slurm_run_fastsurfer_gzj557.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=fastsurfer
-#SBATCH --output=fastsurfer_%A_%a.out
-#SBATCH --error=fastsurfer_%A_%a.err
-#SBATCH --array=1-2%1   # Array job for 2740 MRI files, limit to 5 jobs running at once
+#SBATCH --output=slurm_output/run_fastsurfer/fastsurfer_%A_%a.out
+#SBATCH --error=slurm_output/run_fastsurfer/fastsurfer_%A_%a.err
+#SBATCH --array=1-5%5   # Array job for 2740 MRI files, limit to 5 jobs running at once
 #SBATCH --gres=gpu:1       # Request 1 GPU per job
 #SBATCH --cpus-per-task=2  # Number of CPUs for each task
 #SBATCH --mail-type=ALL    # Mail events (NONE, BEGIN, END, FAIL, ALL)
@@ -30,3 +30,6 @@ cp ~/fastsurfer-output/${SUBJECT_ID}/mri/orig_nu.mgz ~/final_dataset/fs_scans/${
 
 # Reorient the NIfTI file
 python ~/thesis_code/thesis_code/data_collection/reorient_nii.py ~/final_dataset/fs_scans/${SUBJECT_ID}.mgz
+
+# Remove the FastSurfer output
+rm -r ~/final_dataset/fs_scans/${SUBJECT_ID}.mgz
