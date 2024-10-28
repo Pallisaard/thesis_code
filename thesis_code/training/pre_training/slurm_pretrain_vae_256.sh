fe0d666c556c7bd2e382c0630f488c14b51cdf59
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-10-28 16:23:45 +0100

    made a slurm script for the vae 256

--- thesis_code/training/pre_training/slurm_pretrain_vae_256.sh
+++ thesis_code/training/pre_training/slurm_pretrain_vae_256.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=slurm_pretrain_vae_64
-#SBATCH --gres=gpu:4       # Request 4 GPU per job
-#SBATCH --cpus-per-task=6 # Number of CPUs for each gpu
-#SBATCH --mem=32G          # Memory request
+#SBATCH --job-name=pretrain_vae_256
+#SBATCH --output=slurm_pretrain_vae_256-%j.out # Name of output file
+#SBATCH --error=slurm_pretrain_vae_256-%j.err # Name of error file
+#SBATCH --gres=gpu:1       # Request 4 GPU per job
+#SBATCH --cpus-per-task=2  # Number of CPUs for each gpu
+#SBATCH --mem=16G          # Memory request
 #SBATCH --mail-type=ALL    # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=rpa@di.ku.dk # Email
 
@@ -11,21 +13,21 @@ module load cudnn/8.6.0
 
 source ~/venv/bin/activate
 
-python pretrain --model-name "cicek-3d-vae" \
+cd ~/thesis_code
+
+python -m thesis_code.training.pre_training.pretrain --model-name "cicek_3d_vae_256" \
                 --latent-dim 1024 \
-                --data-path "~/final_dataset/fs_scans" \
-                --batch-size 64 \
-                --num-workers 4 \
-                --transforms "resize" "range-normalize" \
-                --resize-size 256 \
+                --data-path ~/final_dataset/fs_scans \
+                --batch-size 8 \
+                --n-workers 0 \
+                --transforms range-normalize \
                 --normalize-min 0 \
                 --normalize-max 1 \
-                --accelerator 'gpu' \
-                --strategy 'ddp' \
-                --devices 4 \
+                --accelerator gpu \
+                --strategy ddp \
+                --devices auto \
                 --max-epochs 100 \
-                --callbacks "checkpoint" "summary" "progress" \
-                --checkpoint-dir "~/lightning/checkpoints" \
+                --callbacks checkpoint summary progress \
                 --save-top-k 3 \
                 --save-last \
                 --fast-dev-run \
