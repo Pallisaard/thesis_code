58ce9ae000dbda22aa5741e4804c93b9af3dfdf5
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-10-28 21:17:12 +0100

    lowered vae-256 batch size to 4

--- thesis_code/training/pre_training/slurm_pretrain_vae_256.sh
+++ thesis_code/training/pre_training/slurm_pretrain_vae_256.sh
@@ -18,7 +18,7 @@ cd ~/thesis_code
 python -m thesis_code.training.pre_training.pretrain --model-name "cicek_3d_vae_256" \
                 --latent-dim 1024 \
                 --data-path ~/final_dataset \
-                --batch-size 8 \
+                --batch-size 4 \
                 --num-workers 0 \
                 --transforms resize range-normalize \
                 --resize-size 256 \
