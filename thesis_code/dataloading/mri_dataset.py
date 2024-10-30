12397806f43aec03111d02d3cf6ab90b516310bb
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-10-30 15:07:11 +0100

    tweaked the mri loading, should work now.

--- thesis_code/dataloading/mri_dataset.py
+++ thesis_code/dataloading/mri_dataset.py
@@ -25,10 +25,8 @@ class MRIDataset(Dataset):
         self.samples: list[Path] = self._load_dataset(self.data_path)
         self.brain_masks: list[Path] = self._load_dataset(self.data_path / "masks")
 
-    def get_brain_mask(self, mri_path: Path) -> torch.Tensor:
-        # Brain stripping code
-        brain_mask_file_name = mri_path.stem.replace(".nii", "") + "_mask.nii.gz"
-        return load_nifti(self.data_path / "masks" / brain_mask_file_name)
+    def get_brain_mask(self, brain_mask_path: Path) -> torch.Tensor:
+        return load_nifti(self.data_path / "masks" / brain_mask_path)
 
     def apply_brain_mask(
         self, mri: torch.Tensor, brain_mask: torch.Tensor
