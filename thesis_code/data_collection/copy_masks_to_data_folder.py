ef9fd5a30a2cebda5035ceca67916b9d674d59dc
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-10-30 16:37:11 +0100

    added code to apply mask to data and cipy to mask folder instead of copying masks.

--- thesis_code/data_collection/copy_masks_to_data_folder.py
+++ thesis_code/data_collection/copy_masks_to_data_folder.py
@@ -25,65 +25,78 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-args = parse_args()
+def apply_mask_to_mri(mask_img, original_mri):
+    reoriented_mask_img = reorient_nii_to_ras(mask_img)
+    reoriented_mask_data = reoriented_mask_img.get_fdata()
+    original_mri_data = original_mri.get_fdata()  # type: ignore
+    masked_mri_data = original_mri_data * reoriented_mask_data
+    masked_mri = nib.Nifti1Image(masked_mri_data, original_mri.affine)  # type: ignore
+    return masked_mri
 
-if not os.path.exists(args.data_dir):
-    raise FileNotFoundError(f"Data directory not found: {args.data_dir}")
 
-data_dir = Path(args.data_dir)
-fastsurfer_output_dir = Path(args.fastsurfer_output_dir)
+if __name__ == "__main__":
+    args = parse_args()
 
-# Define source directories
-source_dirs = {
-    "train": data_dir / "train",
-    "test": data_dir / "test",
-    "val": data_dir / "val",
-}
+    if not os.path.exists(args.data_dir):
+        raise FileNotFoundError(f"Data directory not found: {args.data_dir}")
 
-# Define destination directory
-dest_dir = data_dir / "masks"
+    data_dir = Path(args.data_dir)
+    fastsurfer_output_dir = Path(args.fastsurfer_output_dir)
 
-# Ensure destination directory exists
-dest_dir.mkdir(parents=True, exist_ok=True)
+    # Define source directories
+    source_dirs = {
+        "train": data_dir / "train",
+        "test": data_dir / "test",
+        "val": data_dir / "val",
+    }
 
-# Iterate through each source directory
-for category, source_dir in tqdm(source_dirs.items(), desc="Categories"):
-    # Create category subdirectory in destination
-    category_dest_dir = dest_dir / category
-    category_dest_dir.mkdir(parents=True, exist_ok=True)
+    # Define destination directory
+    dest_dir = data_dir / "masks"
 
-    # Get generator of all .nii files in the source directory
-    mask_paths = source_dir.glob("*.nii.gz")
-    num_files = sum(1 for _ in source_dir.glob("*.nii.gz"))
+    # Ensure destination directory exists
+    dest_dir.mkdir(parents=True, exist_ok=True)
 
-    # Iterate through each file in the source directory
-    for nii_file in tqdm(
-        mask_paths, total=num_files, desc=f"Processing {category}", leave=False
-    ):
-        # Extract the filename without extension
-        file_stem = nii_file.stem.replace(".nii", "")
+    # Iterate through each source directory
+    for category, source_dir in tqdm(source_dirs.items(), desc="Categories"):
+        # Create category subdirectory in destination
+        category_dest_dir = dest_dir / category
+        category_dest_dir.mkdir(parents=True, exist_ok=True)
 
-        # Construct the path to the mask.mgz file
-        mask_path: Path = fastsurfer_output_dir / file_stem / "mri" / "mask.mgz"
+        # Get generator of all .nii files in the source directory
+        mask_paths = source_dir.glob("*.nii.gz")
+        num_files = sum(1 for _ in source_dir.glob("*.nii.gz"))
 
-        # Check if the mask file exists
-        if not mask_path.exists():
-            print(f"Mask file not found: {mask_path}")
-            raise FileNotFoundError(f"Mask file not found: {mask_path}")
+        # Iterate through each file in the source directory
+        for nii_file in tqdm(
+            mask_paths, total=num_files, desc=f"Processing {category}", leave=False
+        ):
+            # Extract the filename without extension
+            nii_file_stem = nii_file.stem.replace(".nii", "")
 
-        # Construct the destination path
-        dest_path: Path = category_dest_dir / f"{file_stem}_mask.nii.gz"
+            # Construct the path to the mask.mgz file
+            mask_path: Path = fastsurfer_output_dir / nii_file_stem / "mri" / "mask.mgz"
 
-        if dest_path.exists():
-            print(f"Mask already exists: {dest_path}")
-            continue
+            # Check if the mask file exists
+            if not mask_path.exists():
+                print(f"Mask file not found: {mask_path}")
+                raise FileNotFoundError(f"Mask file not found: {mask_path}")
 
-        # Load the mask.mgz file using nibabel
-        mask_img = nib.load(mask_path)  # type: ignore
+            # Construct the destination path
+            dest_path: Path = category_dest_dir / f"{nii_file_stem}_mask.nii.gz"
 
-        reoriented_mask_img = reorient_nii_to_ras(mask_img)
+            if dest_path.exists():
+                print(f"Mask already exists: {dest_path}")
+                continue
 
-        # Save the loaded mask to the destination path in .nii.gz format
-        nib.save(reoriented_mask_img, str(dest_path))  # type: ignore
+            # Load the mask.mgz file using nibabel
+            mask_img = nib.load(mask_path)  # type: ignore
+            # Load the original T1w mri
+            original_mri = nib.load(nii_file)  # type: ignore
 
-        # print(f"Saved mask to: {dest_path}")
+            reoriented_mask_img = reorient_nii_to_ras(mask_img)
+            masked_mri = apply_mask_to_mri(reoriented_mask_img, original_mri)
+
+            # Save the loaded mask to the destination path in .nii.gz format
+            nib.save(masked_mri, str(dest_path))  # type: ignore
+
+            # print(f"Saved mask to: {dest_path}")
