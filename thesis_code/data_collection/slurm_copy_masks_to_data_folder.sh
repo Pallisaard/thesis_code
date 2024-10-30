6dad98a61839b9a530e719324be0446dd0c83587
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-10-30 11:50:34 +0100

    i cannot live with the vpn delays, i'll do a slurm script instead

---
+++ thesis_code/data_collection/slurm_copy_masks_to_data_folder.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=copy-files
+#SBATCH --output=copy-files-%j.out # Name of output file
+#SBATCH --error=copy-files-%j.err # Name of error file
+#SBATCH --cpus-per-task=2  # Number of CPUs for each gpu
+#SBATCH --mem=8G          # Memory request
+#SBATCH --mail-type=ALL    # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=rpa@di.ku.dk # Email
+
+source ~/venv/bin/activate
+
+cd ~/thesis_code/
+
+python -m thesis_code.data_collection.copy_masks_to_data_folder --data-path ~/final_dataset --fastsurfer-output-dir ~/fastsurfer_output
