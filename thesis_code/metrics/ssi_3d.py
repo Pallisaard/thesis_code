ecde5e7c9140139b9648b56053043f0dd762993c
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-11-12 17:14:34 +0100

    (fix) better ssi errors.

--- thesis_code/metrics/ssi_3d.py
+++ thesis_code/metrics/ssi_3d.py
@@ -69,7 +69,9 @@ def batch_ssi_3d(
     window_size: int = 11,
     size_average: bool = True,
 ) -> torch.Tensor:
-    assert vols1.size() == vols2.size(), "Input volumes must have the same shape"
+    assert (
+        vols1.size() == vols2.size()
+    ), f"Input volumes must have the same shape, but got {vols1.size()} and {vols2.size()}"
     assert len(vols1.size()) == 5, "Input volumes must have shape (B, C, D, H, W)"
 
     _batch_ssi_3d = torch.vmap(
