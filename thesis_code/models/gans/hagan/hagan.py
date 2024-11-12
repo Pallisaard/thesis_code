bdc427da9f2515f4a936bddccfec16b9fba2007f
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-11-12 16:51:37 +0100

    (fix continued) tried to remove a torch.no_grad(),

--- thesis_code/models/gans/hagan/hagan.py
+++ thesis_code/models/gans/hagan/hagan.py
@@ -277,12 +277,12 @@ class HAGAN(L.LightningModule):
         return out
 
     def encode(self, x: torch.Tensor) -> torch.Tensor:
-        with torch.no_grad():
-            encoded_crop_i_list = []
-            for crop_idx_i in range(0, 256, 256 // 8):
-                real_images_crop_i = S_H(x, crop_idx_i)
-                encoded_crop_i = self.E(real_images_crop_i)
-                encoded_crop_i_list.append(encoded_crop_i)
-            encoded_crops = torch.cat(encoded_crop_i_list, dim=2).detach()
+        # with torch.no_grad():
+        encoded_crop_i_list = []
+        for crop_idx_i in range(0, 256, 256 // 8):
+            real_images_crop_i = S_H(x, crop_idx_i)
+            encoded_crop_i = self.E(real_images_crop_i)
+            encoded_crop_i_list.append(encoded_crop_i)
+        encoded_crops = torch.cat(encoded_crop_i_list, dim=2).detach()
         z_hat = self.Sub_E(encoded_crops)
         return z_hat
