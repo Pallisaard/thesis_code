52329617f20869f4a879a969bf7051e14c51c2f5
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-11-13 09:49:55 +0100

    Tried to limit train batches to 10% of dataset and set max-epochs=2

--- thesis_code/training/pre_training/pretrain.py
+++ thesis_code/training/pre_training/pretrain.py
@@ -333,9 +333,10 @@ def main():
         max_epochs=args.max_epochs,
         max_steps=args.max_steps,
         max_time=args.max_time,
-        val_check_interval=1000,
+        # val_check_interval=1000,
         callbacks=get_callbacks_from_args(args),
-        # limit_train_batches=0.05,
+        limit_train_batches=0.10,
+        # check_val_every_n_epoch=None,
     )
 
     print("Fitting model")
