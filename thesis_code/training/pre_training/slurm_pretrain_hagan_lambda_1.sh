e4257add17ed88c09db076ebd10dc9f272a1c26f
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-11-13 19:08:44 +0100

    renamed standard hagan slurm to include lambdas=1 and lambdas=5

---
+++ thesis_code/training/pre_training/slurm_pretrain_hagan_lambda_1.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH --job-name=pretrain_vae_256_lambda_1
+#SBATCH --output=slurm_pretrain_hagan-%j.out # Name of output file
+#SBATCH --error=slurm_pretrain_hagan-%j.err # Name of error file
+#SBATCH --gres=gpu:titanrtx:2       # Request 4 GPU per job
+#SBATCH --cpus-per-task=4  # Number of CPUs for each gpu
+#SBATCH --mem=16G        # Memory request
+#SBATCH --mail-type=END    # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=rpa@di.ku.dk # Email
+
+module load cuda/11.8
+module load cudnn/8.6.0
+
+source ~/venv/bin/activate
+
+cd ~/thesis_code
+
+# echo devices and nvidia-smi
+echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+
+python -m thesis_code.training.pre_training.pretrain --model-name "hagan" \
+                --latent-dim 1024 \
+                --data-path /home/gzj557/final_dataset \
+                --batch-size 4 \
+                --num-workers 3 \
+                --transforms resize range-normalize remove-percent-outliers \
+                --outlier-percentile 0.001 \
+                --resize-size 256 \
+                --normalize-min -1 \
+                --normalize-max 1 \
+                --checkpoint-monitor val_g_loss \
+                --accelerator gpu \
+                --strategy ddp_find_unused_parameters_true \
+                --devices auto \
+                --callbacks checkpoint summary progress \
+                --save-top-k 3 \
+                --save-last \
+                --log-every-n-steps 50 \
+                --max-steps 500 \
+                --lambda-1 1.0 \
+                --lambda-2 1.0 \
+                # --fast-dev-run \
--- thesis_code/training/pre_training/slurm_pretrain_hagan_lambda_5.sh
+++ thesis_code/training/pre_training/slurm_pretrain_hagan_lambda_5.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=pretrain_vae_256
+#SBATCH --job-name=pretrain_vae_256_lambda_5
 #SBATCH --output=slurm_pretrain_hagan-%j.out # Name of output file
 #SBATCH --error=slurm_pretrain_hagan-%j.err # Name of error file
-#SBATCH --gres=gpu:titanrtx:1       # Request 4 GPU per job
-#SBATCH --cpus-per-task=6  # Number of CPUs for each gpu
+#SBATCH --gres=gpu:titanrtx:2       # Request 4 GPU per job
+#SBATCH --cpus-per-task=4  # Number of CPUs for each gpu
 #SBATCH --mem=16G        # Memory request
 #SBATCH --mail-type=END    # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=rpa@di.ku.dk # Email
@@ -22,7 +22,7 @@ python -m thesis_code.training.pre_training.pretrain --model-name "hagan" \
                 --latent-dim 1024 \
                 --data-path /home/gzj557/final_dataset \
                 --batch-size 4 \
-                --num-workers 5 \
+                --num-workers 3 \
                 --transforms resize range-normalize remove-percent-outliers \
                 --outlier-percentile 0.001 \
                 --resize-size 256 \
@@ -36,5 +36,7 @@ python -m thesis_code.training.pre_training.pretrain --model-name "hagan" \
                 --save-top-k 3 \
                 --save-last \
                 --log-every-n-steps 50 \
-                --max-steps 10000 \
+                --max-steps 80000 \
+                --lambda-1 5.0 \
+                --lambda-2 5.0 \
                 # --fast-dev-run \
