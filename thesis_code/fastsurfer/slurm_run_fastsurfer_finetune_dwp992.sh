0c9a97bb1f69431e5b49916a0587b63239dc0d10
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-11-20 23:13:59 +0100

    also fixed yet another path

--- thesis_code/fastsurfer/slurm_run_fastsurfer_finetune_dwp992.sh
+++ thesis_code/fastsurfer/slurm_run_fastsurfer_finetune_dwp992.sh
@@ -27,7 +27,7 @@ bash ~/projects/thesis/thesis-code/thesis_code/fastsurfer/run_fastsurfer_finetun
 cp ~/projects/thesis/fastsurfer-output/${SUBJECT_ID}/mri/orig_nu.mgz ~/projects/thesis/data/finetune/fs_scans/${SUBJECT_ID}.mgz
 
 # Reorient the NIfTI file
-python ~/thesis_code/thesis_code/data_collection/reorient_nii.py ~/projects/thesis/data/finetune/fs_scans/${SUBJECT_ID}.mgz
+python ~/thesis_code/thesis-code/data_collection/reorient_nii.py ~/projects/thesis/data/finetune/fs_scans/${SUBJECT_ID}.mgz
 
 # Remove the FastSurfer output
 rm -r ~projects/thesis/data/finetune/fs_scans/${SUBJECT_ID}.mgz
