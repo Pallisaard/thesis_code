473ed7c7966b0ac0d615c2571de572713422eabd
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-11-24 14:02:54 +0100

    added resnet model

---
+++ thesis_code/models/medical_net/__init__.py
@@ -0,0 +1,23 @@
+from .resnet import (
+    ResNet,
+    resnet10,
+    resnet18,
+    resnet34,
+    resnet50,
+    resnet101,
+    resnet152,
+    resnet200,
+    get_resnet,
+)
+
+__all__ = [
+    "ResNet",
+    "resnet10",
+    "resnet18",
+    "resnet34",
+    "resnet50",
+    "resnet101",
+    "resnet152",
+    "resnet200",
+    "get_resnet",
+]
