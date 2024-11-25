b6307b6307c84f280cb720661318706cc19be6a5
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-11-25 19:59:49 +0100

    Changed script, might work now.

--- thesis_code/training/evaluation/bash_generate_n_sampled_mris.sh
+++ thesis_code/training/evaluation/bash_generate_n_sampled_mris.sh
@@ -1,3 +1,5 @@
+source ~/venv/bin/activate
+
 python -m thesis_code.training.evaluation.generate_n_sampled_mris --output-dir ../torch-output/generated-examples \
                 --n-samples 200 \
                 --checkpoint-path ../torch-output/pretraining/lightning/checkpoints/last.ckpt \
