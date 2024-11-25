9a28ae2cbad6eaa5a42fcdfa08868b83d74dda87
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-11-25 23:08:14 +0100

    fixed bad path for checkpoints

--- thesis_code/training/evaluation/slurm_generate_n_sampled_mris.sh
+++ thesis_code/training/evaluation/slurm_generate_n_sampled_mris.sh
@@ -15,7 +15,7 @@ source .venv/bin/activate
 
 python -m thesis_code.training.evaluation.generate_n_sampled_mris --output-dir ../torch-output/generated-examples \
                 --n-samples 200 \
-                --checkpoint-path ../torch-output/pretraining/lightning/checkpoints/last.ckpt \
+                --checkpoint-path ../torch-output/pretraining/lightning/checkpoints/hagan_1/last.ckpt \
                 --device 'cuda' \
                 --lambdas 5 \
                 --batch-size 4 \
