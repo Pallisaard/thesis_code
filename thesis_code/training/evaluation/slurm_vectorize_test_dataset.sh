719308f8b40cd46618b78a372117c9bd791e33fa
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-11-26 18:46:07 +0100

    added slurm_ to out and err name

--- thesis_code/training/evaluation/slurm_vectorize_test_dataset.sh
+++ thesis_code/training/evaluation/slurm_vectorize_test_dataset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=vectorize_test_dataset
-#SBATCH --output=vectorize_test_dataset-%j.out # Name of output file
-#SBATCH --error=vectorize_test_dataset-%j.err # Name of error file
+#SBATCH --output=slurm_vectorize_test_dataset-%j.out # Name of output file
+#SBATCH --error=slurm_vectorize_test_dataset-%j.err # Name of error file
 #SBATCH --gres=gpu:titanrtx:1       # Request 4 GPU per job
 #SBATCH --cpus-per-task=4  # Number of CPUs for each gpu
 #SBATCH --mem=32G        # Memory request
