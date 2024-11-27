66c6cb5d2e4b293ca5792498dbd7a81fb5d0add8
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-11-27 15:25:02 +0100

    fixed bad import

--- thesis_code/dataloading/__init__.py
+++ thesis_code/dataloading/__init__.py
@@ -1,7 +1,6 @@
 from .mri_datamodule import MRIDataModule
 from .transforms import MRITransform, Compose, Resize, ZScoreNormalize
 from .mri_dataset import MRIDataset
-from .mri_sample import MRISample
 from .mri_dataloader import (
     get_val_dataset,
     get_train_dataset,
@@ -12,7 +11,6 @@ from .mri_dataloader import (
 __all__ = [
     "MRIDataModule",
     "MRIDataset",
-    "MRISample",
     "MRITransform",
     "Compose",
     "Resize",
