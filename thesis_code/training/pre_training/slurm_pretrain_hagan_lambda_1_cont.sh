f6b3aeb224f81013277a2edb0338ac5a3422c7a9
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-12-08 11:31:31 +0100

    Fixed the name proper

--- thesis_code/training/pre_training/slurm_pretrain_hagan_lambda_1_cont.sh
+++ thesis_code/training/pre_training/slurm_pretrain_hagan_lambda_1_cont.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=cont_pretrain_lambda_1_320k
-#SBATCH --output=slurm_cont_pretrain_hagan_lambda_1_320k-%j.out # Name of output file
-#SBATCH --error=slurm_cont_pretrain_hagan_lambda_1_320k-%j.err # Name of error file
+#SBATCH --output=slurm_cont_pretrain_lambda_1_320k-%j.out # Name of output file
+#SBATCH --error=slurm_cont_pretrain_lambda_1_320k-%j.err # Name of error file
 #SBATCH --gres=gpu:a100:1       # Request 4 GPU per job
 #SBATCH --cpus-per-task=10  # Number of CPUs for each gpu
 #SBATCH --time=1-12:00:00    # Limit to 36 hours.
