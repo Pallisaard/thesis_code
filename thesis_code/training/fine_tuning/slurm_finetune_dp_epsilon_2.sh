fd45e1c928b194b3de041090c4cc57be8ab5a4c7
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-12-10 01:01:56 +0100

    now uses safe dp test mdel. Also set time limit on the model.

--- thesis_code/training/fine_tuning/slurm_finetune_dp_epsilon_2.sh
+++ thesis_code/training/fine_tuning/slurm_finetune_dp_epsilon_2.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=slurm_finetune_dp_e2-%j.out # Name of output file
 #SBATCH --error=slurm_finetune_dp_e2-%j.err # Name of error file
 #SBATCH --gres=gpu:a100:1       # Request 4 GPU per job
-#SBATCH --time=12:00:00       # Time limit day-hrs:min:sec
+#SBATCH --time=3:00:00       # Time limit day-hrs:min:sec
 #SBATCH --cpus-per-task=10  # Number of CPUs for each gpu
 #SBATCH --mem=16G        # Memory request
 #SBATCH --mail-type=END    # Mail events (NONE, BEGIN, END, FAIL, ALL)
@@ -33,7 +33,7 @@ CUDA_LAUNCH_BLOCKING=1 python -m thesis_code.training.fine_tuning.finetune  --la
                 --batch-size 4 \
                 --num-workers 8 \
                 --device 'auto' \
-                --load-from-checkpoint '../checkpoints/pretrained/all-data'/hagan_l1_320k.ckpt \
+                --load-from-checkpoint ../checkpoints/pretrained/safe_dp_test.ckpt \
                 --val-every-n-steps 1000 \
                 --checkpoint-every-n-steps 2500 \
                 --checkpoint-path 'checkpoints/finetuned/no_dp' \
