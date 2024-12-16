ddc4fff13c25aed2ef4e79b63ba9c8fa25da0e10
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-12-16 16:06:28 +0100

    Readied vae-256 for a new run, added a new line to lambda 5

--- thesis_code/training/pre_training/slurm_pretrain_vae_256.sh
+++ thesis_code/training/pre_training/slurm_pretrain_vae_256.sh
@@ -2,33 +2,40 @@
 #SBATCH --job-name=pretrain_vae_256
 #SBATCH --output=slurm_pretrain_vae_256-%j.out # Name of output file
 #SBATCH --error=slurm_pretrain_vae_256-%j.err # Name of error file
-#SBATCH --gres=gpu:titanrtx:1       # Request 4 GPU per job
-#SBATCH --cpus-per-task=6  # Number of CPUs for each gpu
-#SBATCH --mem=16G          # Memory request
-# #SBATCH --mail-type=ALL    # Mail events (NONE, BEGIN, END, FAIL, ALL)
-# #SBATCH --mail-user=rpa@di.ku.dk # Email
+#SBATCH --gres=gpu:a100:1       # Request 4 GPU per job
+#SBATCH --cpus-per-task=10  # Number of CPUs for each gpu
+#SBATCH --time=1-12:00:00    # Limit to 36 hours.
+#SBATCH --mem=16G        # Memory request
+#SBATCH --mail-type=END    # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=rpa@di.ku.dk # Email
 
 module load cuda/11.8
 module load cudnn/8.6.0
+module load gcc/13.2.0
 
-source ~/venv/bin/activate
+cd ~/projects/thesis/thesis-code
+source .venv/bin/activate
 
-cd ~/thesis_code
+# echo devices and nvidia-smi
+echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+
+# echo time at start
+echo "start time: $(date)"
 
 python -m thesis_code.training.pre_training.pretrain --model-name "cicek_3d_vae_256" \
                 --latent-dim 1024 \
-                --data-path /home/gzj557/final_dataset \
-                --batch-size 2 \
-                --num-workers 5 \
-                --transforms resize range-normalize \
-                --resize-size 256 \
-                --normalize-min 0 \
-                --normalize-max 1 \
+                --data-path ../data/pre-training/brain-masked  \
+                --use-all-data-for-training \
+                --batch-size 4 \
+                --num-workers 8 \
                 --accelerator gpu \
-                --strategy ddp \
                 --devices auto \
-                --max-epochs 100 \
-                --callbacks checkpoint summary progress \
+                --callbacks 'checkpoint' \
                 --save-top-k 3 \
                 --save-last \
-                --fast-dev-run \
+                --log-every-n-steps 50 \
+                --max-steps 280000 \  # 70.000 steps of 4 per batch
+                # --fast-dev-run \
+
+# echo time at end
+echo "end time: $(date)"
