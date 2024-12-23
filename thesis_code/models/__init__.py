262e3255fa335df9cd384a25ef78a8588b9f7af4
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-12-23 16:04:57 +0100

    fixed bad imports

--- thesis_code/models/__init__.py
+++ thesis_code/models/__init__.py
@@ -2,22 +2,22 @@ from .vaes import LitVAE3D
 from .unused_models import (
     ResNet,
     resnet50,
-    LitKwonGan,
     LitAlphaGAN,
     LitWGANGP,
     LitWGAN,
     LitGAN,
 )
+from .gans import LitHAGAN, LitKwonGan
 
 
 __all__ = [
     "LitVAE3D",
-    "LitKwonGan",
     "ResNet",
     "resnet50",
     "LitAlphaGAN",
     "LitWGANGP",
     "LitWGAN",
     "LitGAN",
+    "LitHAGAN",
     "LitKwonGan",
 ]
