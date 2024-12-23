6c890d48a1d3487bf408915cda4f90a8b2d3806e
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-12-23 16:40:23 +0100

    fixed bad interpolation

--- thesis_code/models/gans/kwon/kwon_gan.py
+++ thesis_code/models/gans/kwon/kwon_gan.py
@@ -292,8 +292,8 @@ class LitKwonGan(L.LightningModule):
     def _interpolate_data_with_gradient(
         self, real_data: torch.Tensor, fake_data: torch.Tensor
     ) -> torch.Tensor:
-        alpha = torch.rand(real_data.size(0), 1, 1, 1, device=self.device)
-        alpha = alpha.expand_as(real_data)
+        alpha = torch.rand(real_data.size(0), device=self.device)
+        # alpha = alpha.expand_as(real_data)
         interpolates = alpha * real_data + (1 - alpha) * fake_data
         interpolates.requires_grad_(True)
         return interpolates
