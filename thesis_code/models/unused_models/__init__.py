d6b043e53dda86ffcd4bab344c1c52ef367f8fe7
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-12-23 16:02:58 +0100

    fixed bad commit

--- thesis_code/models/unused_models/__init__.py
+++ thesis_code/models/unused_models/__init__.py
@@ -1,5 +1,5 @@
 from .medical_net import ResNet, resnet50
-from .alt import LitKwonGan, LitAlphaGAN, LitWGANGP, LitWGAN, LitGAN
+from .alt import LitAlphaGAN, LitWGANGP, LitWGAN, LitGAN
 
 __all__ = [
     "ResNet",
@@ -8,5 +8,4 @@ __all__ = [
     "LitWGANGP",
     "LitWGAN",
     "LitGAN",
-    "LitKwonGan",
 ]
