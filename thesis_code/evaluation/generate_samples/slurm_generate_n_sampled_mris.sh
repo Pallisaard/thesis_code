c1e50cc031e1e70b1519700cf21c967a4e18853b
Rasmus Pallisgaard <rasm796h@gmail.com>
2025-01-06 16:50:14 +0100

    chagned to gpu that is available.

--- thesis_code/evaluation/generate_samples/slurm_generate_n_sampled_mris.sh
+++ thesis_code/evaluation/generate_samples/slurm_generate_n_sampled_mris.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=slurm_generate_n_sampled_mris_authors-%j.out # Name of output file
 #SBATCH --error=slurm_generate_n_sampled_mris_authors-%j.err # Name of error file
 #SBATCH --array=1-4%4   # Array job for 2740 MRI files, limit to 5 jobs running at once
-#SBATCH --gres=gpu:titanrtx:1       # Request 4 GPU per job
+#SBATCH --gres=gpu:a100:1       # Request 4 GPU per job
 #SBATCH --time=01:15:00    # Time limit hrs:min:sec
 #SBATCH --cpus-per-task=4  # Number of CPUs for each gpu
 #SBATCH --mem=32G        # Memory request
@@ -24,7 +24,10 @@ if [ $SLURM_ARRAY_TASK_ID -eq 1 ]; then
                     --batch-size 2 \
                     --from-authors \
                     --vectorizer-dim 512
+
 elif [ $SLURM_ARRAY_TASK_ID -eq 2 ]; then
+    print("This is the second task")
+    sleep 10
     python -m thesis_code.evaluation.generate_samples.generate_n_sampled_mris --output-dir ../torch-output/pretrain-eval/generated-examples-lambda-1 \
                     --n-samples 512 \
                     --use-dp-safe \
@@ -33,6 +36,7 @@ elif [ $SLURM_ARRAY_TASK_ID -eq 2 ]; then
                     --device auto \
                     --batch-size 2 \
                     --vectorizer-dim 512
+
 elif [ $SLURM_ARRAY_TASK_ID -eq 3 ]; then
     python -m thesis_code.evaluation.generate_samples.generate_n_sampled_mris --output-dir ../torch-output/pretrain-eval/generated-examples-lambda-5 \
                     --n-samples 512 \
@@ -42,6 +46,7 @@ elif [ $SLURM_ARRAY_TASK_ID -eq 3 ]; then
                     --device auto \
                     --batch-size 2 \
                     --vectorizer-dim 512
+
 elif [ $SLURM_ARRAY_TASK_ID -eq 4 ]; then
     python -m thesis_code.evaluation.generate_samples.vectorize_test_dataset --data-dir ../data/pre-training/brain-masked \
                     --output-dir ../torch-output/pretrain-eval/'true-examples-all' \
@@ -49,6 +54,7 @@ elif [ $SLURM_ARRAY_TASK_ID -eq 4 ]; then
                     --make-filename-file \
                     --out-vectorizer-name mri_vectorizer_all_512_out.npy \
                     --vectorizer-dim 512
+
 else
     echo "Invalid SLURM_ARRAY_TASK_ID"
     exit
