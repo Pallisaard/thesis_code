89fda24a1aeb8c974ce089bfebe2f2f8f1d190c2
Rasmus Pallisgaard <rasm796h@gmail.com>
2025-01-15 18:00:59 +0100

    also added a timer as otherwise it won't run.

--- thesis_code/training/pre_training/slurm_pretrain_vae_64.sh
+++ thesis_code/training/pre_training/slurm_pretrain_vae_64.sh
@@ -4,6 +4,7 @@
 #SBATCH --error=slurm_pretrain_vae_64-%j.err # Name of error file
 #SBATCH --gres=gpu:a100:1       # Request 4 GPU per job
 #SBATCH --cpus-per-task=10  # Number of CPUs for each gpu
+#SBATCH --time=24:00:00    # Limit to 36 hours.
 #SBATCH --mem=32G          # Memory request
 #SBATCH --mail-type=ALL    # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=rpa@di.ku.dk # Email
