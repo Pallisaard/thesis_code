bd6e062a5f8f844a1675e40e807088ebc5fe86dc
Rasmus Pallisgaard <rasm796h@gmail.com>
2025-01-15 17:59:41 +0100

    might as well train a 64^3 vae

--- thesis_code/training/pre_training/slurm_pretrain_vae_64.sh
+++ thesis_code/training/pre_training/slurm_pretrain_vae_64.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=pretrain_vae_64
 #SBATCH --output=slurm_pretrain_vae_64-%j.out # Name of output file
 #SBATCH --error=slurm_pretrain_vae_64-%j.err # Name of error file
-#SBATCH --gres=gpu:titanrtx:1       # Request 4 GPU per job
-#SBATCH --cpus-per-task=5  # Number of CPUs for each gpu
-#SBATCH --mem=16G          # Memory request
+#SBATCH --gres=gpu:a100:1       # Request 4 GPU per job
+#SBATCH --cpus-per-task=10  # Number of CPUs for each gpu
+#SBATCH --mem=32G          # Memory request
 #SBATCH --mail-type=ALL    # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=rpa@di.ku.dk # Email
 
@@ -16,20 +16,16 @@ source ~/venv/bin/activate
 cd ~/thesis_code
 
 python -m thesis_code.training.pre_training.pretrain --model-name "cicek_3d_vae_64" \
-                --latent-dim 256 \
-                --data-path /home/gzj557/final_dataset \
+                --latent-dim 1024 \
+                --data-path ../data/pre-training/brain-masked-zerosliced-64 \
+                --use-all-data-for-training \
                 --batch-size 32 \
-                --strip-skulls \
-                --num-workers 4 \
-                --transforms resize range-normalize \
+                --num-workers 8 \
                 --resize-size 64 \
-                --normalize-min 0 \
-                --normalize-max 1 \
                 --accelerator gpu \
-                --strategy ddp \
                 --devices auto \
                 --max-epochs 100 \
                 --callbacks checkpoint summary \
                 --save-top-k 3 \
                 --save-last \
-                --log-every-n-steps 10 \
+                --log-every-n-steps 50 \
