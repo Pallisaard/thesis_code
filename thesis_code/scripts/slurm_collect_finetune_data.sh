ac5566d3e326017d9b052124eb39fd8909e1f0a1
Rasmus Pallisgaard <rasm796h@gmail.com>
2025-01-19 21:27:47 +0100

    added slurm script for collecting finetune data

---
+++ thesis_code/scripts/slurm_collect_finetune_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=collect-finetune-data # Job name
+#SBATCH --output=collect-finetune-data-%j.out # Name of output file
+#SBATCH --error=collect-finetune-data-%j.err # Name of error file
+#SBATCH --ntasks=1 # Number of tasks
+#SBATCH --cpus-per-task=2 # Number of CPU cores per task
+#SBATCH --time=6:00:00 # Wall time
+#SBATCH --mem-per-cpu=16000 # Memory per CPU core
+#SBATCH --mail-user=rpa@di.ku.dk # Email
+#SBATCH --mail-type=END # When to email
+
+cd ~/home/projects/thesis/thesis-code
+conda activate thesis
+bash thesis_code/data-collection/collect_finetune_data.sh ../data/fine-tuning
