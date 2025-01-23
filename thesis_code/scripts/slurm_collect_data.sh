59b1015a181911c12dd39e9353915eddfdd10cfd
Rasmus Pallisgaard <rasm796h@gmail.com>
2025-01-23 21:50:38 +0100

    updated name

--- thesis_code/scripts/slurm_collect_data.sh
+++ thesis_code/scripts/slurm_collect_data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=gather-pretraining-data # Job name
-#SBATCH --output=gather-pretraining-data-%j.out # Name of output file
-#SBATCH --error=gather-pretraining-data-%j.err # Name of error file
+#SBATCH --job-name=collect-pretrain-data # Job name
+#SBATCH --output=slurm_collect_pretrain_data-%j.out # Name of output file
+#SBATCH --error=slurm_collect_pretrain_data-%j.err # Name of error file
 #SBATCH --ntasks=1 # Number of tasks
 #SBATCH --cpus-per-task=12 # Number of CPU cores per task
 #SBATCH --time=1-00:00:00 # Wall time
@@ -10,6 +10,6 @@
 #SBATCH --mail-user=rpa@di.ku.dk # Email
 #SBATCH --mail-type=END # When to email
 
-cd ~/projects/thesis/thesis-code
-source .venv/bin/activate
+cd ~/projects/thesis/thesis-code || exit 1
+source .venv/bin/activate || exit 1
 bash thesis_code/data-collection/collect_data.sh ../data/pre-training/collections/
