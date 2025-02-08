7a8dccfe2eeefcb4f02cd3fd5ef0c4b58d40069d
rpa-cai <rpa@component-ai.dk>
2025-02-08 17:38:08 +0100

    Alright now with the save model

--- thesis_code/evaluation/generate_samples/slurm_generate_n_sampled_mris.sh
+++ thesis_code/evaluation/generate_samples/slurm_generate_n_sampled_mris.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=generate_n_sampled_mris
 #SBATCH --output=slurm_generate_n_sampled_mris-%j-%a.out # Name of output file
 #SBATCH --error=slurm_generate_n_sampled_mris-%j-%a.err # Name of error file
-#SBATCH --array=3-3%1
+#SBATCH --array=1-1%1
 #SBATCH --gres=gpu:titanrtx:1
 #SBATCH --time=02:30:00
 #SBATCH --cpus-per-task=4
