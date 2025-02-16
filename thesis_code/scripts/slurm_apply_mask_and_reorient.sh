40f7bf94ceba1457793176ba7161edfb9acffe95
rpa-cai <rpa@component-ai.dk>
2025-02-16 15:41:54 +0100

    now has more cpus

--- thesis_code/scripts/slurm_apply_mask_and_reorient.sh
+++ thesis_code/scripts/slurm_apply_mask_and_reorient.sh
@@ -2,8 +2,9 @@
 #SBATCH --job-name=apply_mask_to_mris
 #SBATCH --output=slurm_apply_mask_to_mris-%j.out # Name of output file
 #SBATCH --error=slurm_apply_mask_to_mris-%j.err # Name of error file
-#SBATCH --cpus-per-task=2  # Number of CPUs for each gpu
-#SBATCH --mem=8G          # Memory request
+#SBATCH --time=01:00:00
+#SBATCH --cpus-per-task=10  # Number of CPUs for each gpu
+#SBATCH --mem=32G          # Memory request
 # #SBATCH --mail-type=ALL    # Mail events (NONE, BEGIN, END, FAIL, ALL)
 # #SBATCH --mail-user=rpa@di.ku.dk # Email
 
@@ -11,7 +12,7 @@ cd ~/projects/thesis/thesis-code/
 
 source .venv/bin/activate
 
-python -m thesis_code.scripts.apply_mask_and_reorient --data-dir ../data/pre-training/not-brain-masked/ --output-dir ../data/pre-training/brain-masked-process-ready-resampled/ --fastsurfer-output-dir ../fastsurfer-output/pre-training/ --use-splits
+python -m thesis_code.scripts.apply_mask_and_reorient --data-dir ../data/pre-training/not-brain-masked/ --output-dir ../data/pre-training/brain-masked-process-ready-resampled/ --fastsurfer-output-dir ../fastsurfer-output/pre-training/ --use-splits --workers 8
 
 
 # --percent-outliers 0.005
