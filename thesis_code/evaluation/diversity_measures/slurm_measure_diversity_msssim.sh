f5c9c61e3a24148421ef3df829ecab5fc161bd38
Rasmus Pallisgaard <rpa@component-ai.dk>
2025-02-18 00:23:11 +0100

    diversity measures ready for everything

--- thesis_code/evaluation/diversity_measures/slurm_measure_diversity_msssim.sh
+++ thesis_code/evaluation/diversity_measures/slurm_measure_diversity_msssim.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=measure_msssim
 #SBATCH --output=slurm_measure_msssim-%j-%a.out
 #SBATCH --error=slurm_measure_msssim-%j-%a.err
-#SBATCH --array=1-1%1
-#SBATCH --gres=gpu:1
+#SBATCH --array=1-6%1
+#SBATCH --gres=gpu:l40s:1
 #SBATCH --time=02:00:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
@@ -30,45 +30,45 @@ if [ $SLURM_ARRAY_TASK_ID -eq 1 ]; then
         --resolution 256 \
         --output-file "$BASE_DIR/generated-examples-hagan-l5-orig/msssim_scores.pt" || { echo "Task 1 failed"; exit 1; }
 
-# elif [ $SLURM_ARRAY_TASK_ID -eq 2 ]; then
-#     echo "HAGAN lambda 5-1"
-#     python -m thesis_code.evaluation.diversity_measures.measure_diversity_msssim \
-#         --input-dir "$BASE_DIR/generated-examples-hagan-l5-1" \
-#         --device cuda \
-#         --resolution 256 \
-#         --output-file "$BASE_DIR/generated-examples-hagan-l5-1/msssim_scores.pt" || { echo "Task 2 failed"; exit 1; }
+elif [ $SLURM_ARRAY_TASK_ID -eq 2 ]; then
+    echo "HAGAN lambda 5-1"
+    python -m thesis_code.evaluation.diversity_measures.measure_diversity_msssim \
+        --input-dir "$BASE_DIR/generated-examples-hagan-l5-1" \
+        --device cuda \
+        --resolution 256 \
+        --output-file "$BASE_DIR/generated-examples-hagan-l5-1/msssim_scores.pt" || { echo "Task 2 failed"; exit 1; }
 
-# elif [ $SLURM_ARRAY_TASK_ID -eq 3 ]; then
-#     echo "WGAN-GP"
-#     python -m thesis_code.evaluation.diversity_measures.measure_diversity_msssim \
-#         --input-dir "$BASE_DIR/generated-examples-wgan-gp" \
-#         --device cuda \
-#         --resolution 64 \
-#         --output-file "$BASE_DIR/generated-examples-wgan-gp/msssim_scores.pt" || { echo "Task 3 failed"; exit 1; }
+elif [ $SLURM_ARRAY_TASK_ID -eq 3 ]; then
+    echo "WGAN-GP"
+    python -m thesis_code.evaluation.diversity_measures.measure_diversity_msssim \
+        --input-dir "$BASE_DIR/generated-examples-wgan-gp" \
+        --device cuda \
+        --resolution 64 \
+        --output-file "$BASE_DIR/generated-examples-wgan-gp/msssim_scores.pt" || { echo "Task 3 failed"; exit 1; }
 
-# elif [ $SLURM_ARRAY_TASK_ID -eq 4 ]; then
-#     echo "Alpha-GAN"
-#     python -m thesis_code.evaluation.diversity_measures.measure_diversity_msssim \
-#         --input-dir "$BASE_DIR/generated-examples-alpha-gan" \
-#         --device cuda \
-#         --resolution 64 \
-#         --output-file "$BASE_DIR/generated-examples-alpha-gan/msssim_scores.pt" || { echo "Task 4 failed"; exit 1; }
+elif [ $SLURM_ARRAY_TASK_ID -eq 4 ]; then
+    echo "Alpha-GAN"
+    python -m thesis_code.evaluation.diversity_measures.measure_diversity_msssim \
+        --input-dir "$BASE_DIR/generated-examples-alpha-gan" \
+        --device cuda \
+        --resolution 64 \
+        --output-file "$BASE_DIR/generated-examples-alpha-gan/msssim_scores.pt" || { echo "Task 4 failed"; exit 1; }
 
-# elif [ $SLURM_ARRAY_TASK_ID -eq 5 ]; then
-#     echo "Kwon-GAN"
-#     python -m thesis_code.evaluation.diversity_measures.measure_diversity_msssim \
-#         --input-dir "$BASE_DIR/generated-examples-kwon-gan" \
-#         --device cuda \
-#         --resolution 64 \
-#         --output-file "$BASE_DIR/generated-examples-kwon-gan/msssim_scores.pt" || { echo "Task 5 failed"; exit 1; }
+elif [ $SLURM_ARRAY_TASK_ID -eq 5 ]; then
+    echo "Kwon-GAN"
+    python -m thesis_code.evaluation.diversity_measures.measure_diversity_msssim \
+        --input-dir "$BASE_DIR/generated-examples-kwon-gan" \
+        --device cuda \
+        --resolution 64 \
+        --output-file "$BASE_DIR/generated-examples-kwon-gan/msssim_scores.pt" || { echo "Task 5 failed"; exit 1; }
 
-# elif [ $SLURM_ARRAY_TASK_ID -eq 6 ]; then
-#     echo "VAE-64"
-#     python -m thesis_code.evaluation.diversity_measures.measure_diversity_msssim \
-#         --input-dir "$BASE_DIR/generated-examples-vae-64" \
-#         --device cuda \
-#         --resolution 64 \
-#         --output-file "$BASE_DIR/generated-examples-vae-64/msssim_scores.pt" || { echo "Task 6 failed"; exit 1; }
+elif [ $SLURM_ARRAY_TASK_ID -eq 6 ]; then
+    echo "VAE-64"
+    python -m thesis_code.evaluation.diversity_measures.measure_diversity_msssim \
+        --input-dir "$BASE_DIR/generated-examples-vae-64" \
+        --device cuda \
+        --resolution 64 \
+        --output-file "$BASE_DIR/generated-examples-vae-64/msssim_scores.pt" || { echo "Task 6 failed"; exit 1; }
 
 else
     echo "Invalid SLURM_ARRAY_TASK_ID"
