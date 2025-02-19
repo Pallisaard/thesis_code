0226e22e46131cd96e8bf4f2d7815de2c1af3501
Rasmus Pallisgaard <rpa@component-ai.dk>
2025-02-19 22:08:10 +0100

    Now has correct name

--- thesis_code/scripts/slurm_preprocess_example-64.sh
+++ thesis_code/scripts/slurm_preprocess_example-64.sh
@@ -32,7 +32,7 @@ fi
 
 echo "dir: "$dir
 
-preprocess_dir="../data/pre-training/brain-masked-zerosliced-64/"$dir
+preprocess_dir="../data/pre-training/brain-masked-no-zerosliced-64/"$dir
 
 echo "preprocess_dir: " $preprocess_dir
 
